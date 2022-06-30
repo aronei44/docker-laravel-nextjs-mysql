c6e3ca83c82b7a42c08e414dfa8842a92298175c
aronei44 <meliodasmeliodas224@gmail.com>
2022-06-30 10:17:00 +0700

    fix git not found file

---
+++ ui/next.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// }
+
+// module.exports = nextConfig
+
+module.exports = {
+  webpackDevMiddleware: config => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    }
+    return config
+  },
+  reactStrictMode: true,
+}
